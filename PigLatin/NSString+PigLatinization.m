5725d016dfd43eb05819aa181a6fc5a2ea7914a5
olga-nest <olga.w.nesterova@gmail.com>
2017-10-06 21:18:17 -0700

    Add a flow for a words that starts with vowel

--- PigLatin/NSString+PigLatinization.m
+++ PigLatin/NSString+PigLatinization.m
@@ -4,14 +4,33 @@
 
 -(NSString *)stringByPigLatinization {
     
-    NSString *translateMe = self;
+    NSString *inputstring = self;
+    NSString *translateMe = [inputstring lowercaseString];
+    
     NSArray *wordsArr = [translateMe componentsSeparatedByString:@" "];
+    NSMutableArray *translatedStringArr = [NSMutableArray new];
     
-    NSArray *vowels = [NSArray arrayWithObjects:@"a", @"e", @"i", @"o", @"u", nil];
+    NSCharacterSet *vowels = [NSCharacterSet characterSetWithCharactersInString:@"aeiou"];
     
+    for (NSString *word in wordsArr) {
+        
+        NSRange range = [word rangeOfCharacterFromSet:vowels];
+        if (range.location == 0) {
+            NSString *translatedWord = [NSString stringWithFormat:@"%@ay", word];
+            [translatedStringArr addObject:translatedWord];
+            NSLog(@"translated str array count %lu", [translatedStringArr count]);
+        } else {
+            NSLog(@"Ooops");
+        }
+    }
     
+    NSMutableString *translatedString = [[NSMutableString alloc] init];
+    for (NSObject * obj in translatedStringArr)
+    {
+        [translatedString appendString:[obj description]];
+    }
     
-    return 0;
+    return translatedString;
 }
 
 @end
