3c7408936af24c2425dff71ce4add4f4d6479059
olga-nest <olga.w.nesterova@gmail.com>
2017-10-06 17:20:00 -0700

    Initial Commit

---
+++ PigLatin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PigLatin
+//
+//  Created by Olga on 10/6/17.
+//  Copyright © 2017 Olga. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
