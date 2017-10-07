b235b576e3b8aba056e3e2aa1401905462dc1d73
olga-nest <olga.w.nesterova@gmail.com>
2017-10-06 19:52:46 -0700

    Add a string to translate

--- PigLatin/main.m
+++ PigLatin/main.m
@@ -1,17 +1,13 @@
-//
-//  main.m
-//  PigLatin
-//
-//  Created by Olga on 10/6/17.
-//  Copyright © 2017 Olga. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
+#import "NSString+PigLatinization.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+
+    NSString *translateMe = @"Using the static storage class designator to limit the scope of na";
+        
+    NSLog(@"%@", [translateMe stringByPigLatinization]);
+        
     }
     return 0;
 }
